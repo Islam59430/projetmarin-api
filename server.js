6064c9742138401944af7a45727549fabb52657f
islam.fargiev2d12@gmail.com <islam.fargiev@STSIRIS.EUROPE>
2026-04-07 14:27:14 +0200

    ajout page html et route carte

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 app.use(express.json());
 
-// Route webhook TTN
-app.post("/api/ttn", (req, res) => {
-    console.log("Message TTN reçu !");
-    console.log(req.body);
-
-    res.status(200).send("OK");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "map.html"));
 });
 
-// Test serveur
-app.get("/", (req, res) => {
-    res.send("Serveur projet marin actif");
+app.post("/api/ttn", (req, res) => {
+  console.log("Message TTN reçu !");
+  console.log(req.body);
+
+  res.status(200).send("OK");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Serveur lancé sur port " + PORT);
+  console.log("Serveur lancé sur port " + PORT);
 });
